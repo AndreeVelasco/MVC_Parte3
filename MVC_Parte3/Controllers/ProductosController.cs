7305c47e40cedd78e4c0a4785c40a6eea01305fa
Andree Velasco <apandcan@gmail.com>
2020-08-31 22:51:19 -0500

    Agregar archivos de proyecto.

---
+++ MVC_Parte3/Controllers/ProductosController.cs
@@ -0,0 +1,34 @@
+﻿using MVC_Parte3.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVC_Parte3.Controllers
+{
+    public class ProductosController : Controller
+    {
+        List<ClsDatos> producto = new List<ClsDatos>();
+        public ProductosController()
+        {
+            ClsDatos obj = new ClsDatos();
+            obj.descripcion = "Manzana";
+            obj.cantidad = 10;
+            obj.precio = 0.50m;
+            producto.Add(obj);
+
+            ClsDatos obj1 = new ClsDatos();
+            obj1.descripcion = "Banana";
+            obj1.cantidad = 15;
+            obj1.precio = 0.80m;
+            producto.Add(obj1);
+        }
+        // GET: Productos
+        public ViewResult MostrarProductos(int id)//sub clase de actionresult
+        {
+            ClsDatos obj = producto[id];
+            return View(obj);
+        }
+    }
+}
---
+++ MVC_Parte3/Models/ClsDatos.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MVC_Parte3.Models
+{
+    public class ClsDatos
+    {
+        public string descripcion { get; set; }
+        public int cantidad { get; set; }
+        public decimal precio { get; set; }
+    }
+}
